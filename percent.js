cf6eca91a5f625a2927f1d43982107b9f34c0517
Davy Peter Braun <hello@davybraun.com>
2016-11-24 12:03:09 +0100

    Add a reset parameter and document 'percent' module.

--- percent.js
+++ percent.js
@@ -1,6 +1,14 @@
 var percent = 0;
 
-module.exports = function (p) {
+/**
+ * Singleton storage of a value used to indicate progress.
+ *
+ * @param  {number}  p     Value in.
+ * @param  {boolean} reset resets value if set true.
+ * @return {number}        Value out.
+ */
+module.exports = function (p, reset) {
   percent = p ? p : percent;
+  if (reset) percent = 0;
   return +percent
 }
