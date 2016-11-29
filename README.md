2345ae027a56f78c5418a640ba3725a882518c9c
Davy Peter Braun <hello@davybraun.com>
2016-11-29 11:08:32 +0100

    Set up Electron app.

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# iat-data-exporter
+# IAT DATA EXPORTER [![XO code style](https://img.shields.io/badge/code_style-XO-5ed9c7.svg)](https://github.com/sindresorhus/xo)
+
 > Given the URI of a CSV, this web app exports from a Heroku dataclip, processes/refines data and make it available for download. Used in the Trustlab project.
+
+
+## Dev
+
+```
+$ npm install
+```
+
+### Run
+
+```
+$ npm start
+```
+
+### Build
+
+```
+$ npm run build
+```
+
+Builds the app for macOS, Linux, and Windows, using [electron-packager](https://github.com/electron-userland/electron-packager).
+
+
+## License
+
+MIT © [<%= name %>](<%= website %>)
--- index.js
+++ index.js
@@ -1,117 +1,44 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var jsonParser = bodyParser.json();
-var downloader = require('./downloader');
-var transformer = require('./transformer');
-var fileMaker = require('./file-maker');
-var eraseFilesFromDir = require('./file-deleter');
-var zipper = require('./file-zipper');
-var app = express();
-var percent = require('./percent');
-var fs = require('fs');
+'use strict';
+const electron = require('electron');
 
-var isFetching = false;
-var isTransforming = false;
-var isReady = false;
-var hasError = false;
-var errorMsg = null;
+const app = electron.app;
 
-var zipFile;
+// adds debug features like hotkeys for triggering dev tools and reload
+require('electron-debug')();
 
-var zipFileName = function () {
-  var d = new Date();
-  return 'IAT_export_' +
-    d.getFullYear().toString() +
-    d.getMonth().toString() +
-    d.getDate().toString() + '_' +
-    d.getHours().toString() +
-    d.getMinutes().toString() +
-    d.getSeconds().toString();
+// prevent window being garbage collected
+let mainWindow;
+
+function onClosed() {
+	// dereference the window
+	// for multiple windows store them in an array
+	mainWindow = null;
 }
 
-app.use('/static', express.static(__dirname + '/public'));
+function createMainWindow() {
+	const win = new electron.BrowserWindow({
+		width: 600,
+		height: 400
+	});
 
-app.set('views', __dirname + '/views');
-app.engine('.html', require('ejs').renderFile)
+	win.loadURL(`file://${__dirname}/index.html`);
+	win.on('closed', onClosed);
 
-app.get('/', function (req, res) {
-  res.render('index.html');
-});
-
-app.post('/fetch', jsonParser, function (req, res) {
-  res.status(200).send({status: 'Fetching data from database...', progress: percent()});
-  triggerFetch(req.body.uri);
-});
-
-app.get('/status', function (req, res) {
-  if (isFetching) {
-    return res.status(200).send({status: 'Fetching data from database...', progress: percent()});
-  } else if (isTransforming) {
-    return res.status(200).send({status: 'Transforming into CSV', progress: 100});
-  } else if (isReady) {
-    return res.status(200).send({status: 'ready', payload: 'static/downloadables/' + zipFile});
-  } else if (hasError) {
-    return res.status(500).send({status: 'error', message: errorMsg});
-  } else {
-    res.status(500);
-  }
-});
-
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Serving on port ' + (process.env.PORT || 3000) + '...');
-});
-
-function triggerFetch(uri) {
-  console.log('---> Triggering fetch -> transform -> zip');
-  csvFileLinks = null;
-  isFetching = true;
-  eraseFilesFromDir(__dirname + '/public/downloadables/');
-
-  downloader(uri)
-    .then(function (data) {
-      return data.promise;
-    })
-    .then(function (pathToFile) {
-      console.log('---> Fetched');
-      percent(0, true);
-      isFetching = false;
-      isTransforming = true;
-      return Promise.resolve(pathToFile);
-    })
-    .then(function (pathToFile) {
-      return transformer(pathToFile);
-    })
-    .then(function (results) {
-      console.log('---> Transformed');
-      isTransforming = false;
-      return fileMaker(results, true);
-    })
-    .then(function (links) {
-      csvFileLinks = links;
-      eraseFilesFromDir(__dirname + '/public/');
-      return Promise.resolve(true);
-    })
-    .then(function () {
-      return zipper(__dirname + '/public/downloadables/', zipFileName());
-    })
-    .then(function (fileName) {
-      console.log('---> Zipped');
-      zipFile = fileName;
-      isReady = true;
-
-      try {
-        fs.unlink(__dirname + csvFileLinks.meta);
-        fs.unlink(__dirname + csvFileLinks.errors);
-        fs.unlink(__dirname + csvFileLinks.results);
-        csvFileLinks = null;
-      } catch (err) {
-        console.log(err);
-      }
-
-      return Promise.resolve(zipFile);
-    })
-    .catch(function (err) {
-      hasError = true;
-      errorMsg = err.message;
-    });
+	return win;
 }
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('activate', () => {
+	if (!mainWindow) {
+		mainWindow = createMainWindow();
+	}
+});
+
+app.on('ready', () => {
+	mainWindow = createMainWindow();
+});
